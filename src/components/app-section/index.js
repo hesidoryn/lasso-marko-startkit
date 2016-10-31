e0eb4aa711f812ba190e84886b34475bcded7112
schetnikovich <tschetnikovich@gmail.com>
2016-10-31 20:00:00 +0300

    Section and theme loader widgets updated

--- src/components/app-section/index.js
+++ src/components/app-section/index.js
@@ -7,7 +7,8 @@ module.exports = require('marko-widgets').defineComponent({
     return {
       open: input.open === undefined ? false : input.open,
       textShow: input.textShow || "Show",
-      textHide: input.textHide || "Hide"
+      textHide: input.textHide || "Hide",
+      height: input.open == true ? "auto" : "0"
     };
   },
 
@@ -15,14 +16,34 @@ module.exports = require('marko-widgets').defineComponent({
     return {
       open: state.open,
       className: className(state.open),
-      linkTitle: state.open ? state.textHide : state.textShow,
-      linkIndicator: state.open ? "&darr;" : "&rarr;"
+      moreClassName: state.open ? "app-section-show-more--open" : "app-section-show-more--closed",
+      linkTitle: state.textShow,
+      linkIndicator: state.open ? "&rarr;" : "&rarr;",
+      height: state.height
     };
   },
 
   toggle: function(e) {
     e.preventDefault();
-    this.setState('open', !this.state.open);
+
+    var newOpen = !this.state.open;
+    this.setState('open', newOpen);
+
+    if (!newOpen) {
+      this.setState('height', "0");
+    } else {
+      var content = this.getEl("content");
+      var contentWrapper = this.getEl("contentWrapper");
+      this.setState('height', contentWrapper.clientHeight + "px");
+    }
+  },
+
+  init: function() {
+    if (this.state.open) {
+      var content = this.getEl("content");
+      var contentWrapper = this.getEl("contentWrapper");
+      this.setState('height', contentWrapper.clientHeight + "px");
+    }
   }
 });
 
@@ -32,12 +53,12 @@ module.exports = require('marko-widgets').defineComponent({
  * @returns {string}
  */
 function className(open) {
-  var className = 'app-section';
+  var className = '';
 
   if (open) {
-    className += ' app-section--open';
+    className += ' app-section-content--open';
   } else {
-    className += ' app-section--closed';
+    className += ' app-section-content--closed';
   }
 
   return className;
