87e003a4c607d730b1bf6bad1c101c3da0c52538
schetnikovich <tschetnikovich@gmail.com>
2016-11-07 21:11:15 +0300

    Assets builder and lasso-page-json tag moved to separate package

--- src/build.js
+++ src/build.js
@@ -1,10 +1,13 @@
 /* eslint no-console: 0 */
 
 require('./configure');
+var extras = require('lasso-marko-extras');
+require('raptor-logging').configure({
+  loggers: {
+    'lasso-marko-extras': 'INFO'
+  }
+});
 
-var async = require('async');
-var path = require('path');
-var lasso = require('lasso');
 
 // Specify any files that needs to be copied to assets folder (build/static)
 var files = [
@@ -19,102 +22,11 @@ var pages = [
   'pages/switcher/pages/faq'
 ];
 
-process(files, pages);
+extras.process(__dirname, files, pages, function(err) {
+  if (err) {
+    console.log('Failed to process assets!');
+    throw err;
+  }
 
-
-// ---------------------------
-// Processing of files and pages
-// ---------------------------
-
-function process(files, pages) {
-  processFiles(files, function(err) {
-    if (err) {
-      throw err;
-    }
-
-    console.log('Files processed!');
-
-    processPages(pages, function(err) {
-      if (err) {
-        throw err;
-      }
-
-      console.log('Build complete!');
-    });
-  });
-}
-
-function processFiles(files, callback) {
-  async.eachSeries(files, function(file, callback) {
-    lasso.lassoResource(path.join(__dirname, file), function(err) {
-      if (err) {
-        console.log('Failed to process file: ', err);
-        callback(err);
-        return;
-      }
-
-      console.log('File "' + file + '" processed.');
-      callback(null);
-    });
-
-  }, function(err) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    callback();
-  });
-}
-
-function processPages(pages, callback) {
-  async.eachSeries(pages, function(page, callback) {
-
-    var from = path.join(__dirname, page);
-    var pagePath = path.join(from, 'page.json');
-
-    var config = {};
-    if (require('fs').existsSync(pagePath)) {
-      config = require(pagePath);
-    }
-
-    config.from = from;
-
-    if (!config.name) {
-      config.name = path.basename(page);
-    }
-
-    if (!config.packagePath) {
-      // Look for an browser.json in the same directory
-      config.packagePath = path.join(from, 'browser.json');
-    }
-
-    if (!config.cacheKey) {
-      // Filename of the compiled template is the default cache key
-      config.cacheKey = path.join(from, 'template.marko.js');
-    }
-
-    lasso.lassoPage(config,
-      function(err) {
-        if (err) {
-          console.log('Failed to lasso page: ', err);
-          callback(err);
-          return;
-        }
-
-        console.log('Page "' + page + '" processed.');
-        callback(null);
-      }
-    );
-
-  }, function(err) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    callback();
-  });
-}
-
-
+  console.log('Assets processed!');
+});
