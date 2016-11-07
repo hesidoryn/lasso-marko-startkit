cf302274b646bdfcb58cb8f336b519182e9971da
schetnikovich <tschetnikovich@gmail.com>
2016-11-08 01:34:05 +0300

    Upgrade to lasso-marko-extras 1.0.8

--- src/build.js
+++ src/build.js
@@ -1,32 +1,24 @@
 /* eslint no-console: 0 */
 
 require('./configure');
-var extras = require('lasso-marko-extras');
+
+// Enable logging for 'lasso-marko-extras'
 require('raptor-logging').configure({
   loggers: {
     'lasso-marko-extras': 'INFO'
   }
 });
 
-
-// Specify any files that needs to be copied to assets folder (build/static)
-var files = [
-  'layouts/base/favicon.ico'
-];
-
-// Specify pages
-var pages = [
-  'pages/index',
-  'pages/switcher/pages/index',
-  'pages/switcher/pages/about',
-  'pages/switcher/pages/faq'
-];
-
-extras.process(__dirname, files, pages, function(err) {
-  if (err) {
-    console.log('Failed to process assets!');
-    throw err;
-  }
-
-  console.log('Assets processed!');
+// Build static assets
+require('lasso-marko-extras').build({
+  baseDir: __dirname,
+  files: [
+    'layouts/base/favicon.ico'
+  ],
+  pages: [
+    'pages/index',
+    'pages/switcher/pages/index',
+    'pages/switcher/pages/about',
+    'pages/switcher/pages/faq'
+  ]
 });
