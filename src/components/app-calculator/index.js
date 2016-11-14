249084145db1233d6a4bd4a32248e9eb76addaed
Heorhi Sidoryn <heorhi.sidoryn@gmail.com>
2016-11-14 19:49:33 +0300

    added simple calculator

--- src/components/app-calculator/index.js
+++ src/components/app-calculator/index.js
@@ -1,37 +1,80 @@
+require('./style.less');
+
 module.exports = require('marko-widgets').defineComponent({
   template: require('./template.marko'),
 
-  getInitialState: function(input) {
+  getInitialState: function() {
     return {
-      name: input.name || "Calculator",
-      first: "",
-      action: "",
-      second: "",
-      equal: "",
-      result: null
+      first: '',
+      action: '',
+      second: '',
+      error: '',
+      wand: true
     };
   },
 
   getTemplateData: function(state) {
+    this.wandAction;
     return {
-      name: state.name,
       first: state.first,
       action: state.action,
       second: state.second,
-      equal: state.equal,
-      result: state.result
+      error: state.error,
+      wand: state.wand
     };
   },
 
-  set: function(number) {
-    if (action == "") {
-      this.setState('first', number)
+  init: function() {
+    const self = this;
+    setInterval(() => {
+      const wand = !self.state.wand;
+      this.setState('wand', wand);
+    }, 500);
+  },
+
+  set: function(e) {
+    if (this.state.error !== '') {
+      this.setState('error', '');
+    }
+    if (this.state.action === '') {
+      var first = this.state.first.toString() + e.target.value;
+      this.setState('first', first);
     } else {
-      this.setState('second', number)
+      var second = this.state.second.toString() + e.target.value;
+      this.setState('second', second);
     }
   },
 
-  setAction: function(action) {
-    this.setState('action', action)
+  setAction: function(e) {
+    if (this.state.error !== '') {
+      this.setState('error', '');
+    }
+    this.setState('action', e.target.value);
+  },
+
+  getResult: function() {
+    if (this.state.second === '') {
+      this.setState('error', 'Malformed expression');
+    } else {
+      try {
+        const result = eval(`${this.state.first}${this.state.action}${this.state.second}`);
+        if (typeof result === 'number' && isFinite(result)) {
+          this.setState('action', '');
+          this.setState('second', '');
+          this.setState('first', result);
+        } else {
+          this.setState('error', 'Malformed expression');
+        }
+      } catch (err) {
+        this.setState('error', 'Malformed expression');
+      }
+    }
+  },
+
+  clear: function() {
+    this.setState('first', '');
+    this.setState('action', '');
+    this.setState('second', '');
+    this.setState('error', '');
   },
 });
